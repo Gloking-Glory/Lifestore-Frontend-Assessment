ff238fd5038d85a11ae2c3a3bde130c9b12c41d4
AYANWOLA GLORY TOLUWANI <ayanwolaglory@gmail.com>
2022-06-21 13:14:26 +0100

    lifestore assessment

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import MaterialUI from './components/MaterialUI';
 import DisplayProduct from './components/DisplayProduct';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={ <MaterialUI />} />
-        <Route exact path="/product" element={ <DisplayProduct />} />
+        <Route exact path="/" element={ <DisplayProduct />} />
       </Routes>
     </div>
   );
